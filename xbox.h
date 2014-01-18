c50012467bab0ec9b988f94d87fff9f1ed38cbdf
AdrS <adrs1118@gmail.com>
2014-01-18 12:53:45 -0500

    added constants for xbox controller buttons

---
+++ xbox.h
@@ -0,0 +1,6 @@
+#ifndef __XBOX_H_
+#define __XBOX_H_
+const unsigned int ABUTTON = 1, BBUTTON = 2, XBUTTON = 3, YBUTTON = 4,
+		LBUMPER = 5, RBUMPER = 6, START = 7, BACK = 8, 
+		LSTICKP = 9, RSTICKP = 10, DPADVERT = 7, DPADSIDE = 6;
+#endif
